be4ab9f510d290645d1d18a4670b89134b4fd851
Kurtis Nusbaum <klnusbaum@gmail.com>
2012-06-09 03:48:11 -0500

    Initial commit for the night night repo. things are kind of working on mac

---
+++ nightNight.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+import os
+import random
+import subprocess
+import argparse
+import platform
+import json
+
+
+def getSettingsFileName():
+  return os.path.join(os.getenv("HOME"), ".night_night_settings")
+
+def getFiles(entry):
+  toReturn = []
+  if os.path.isdir(entry):
+    for file in os.listdir(entry):
+      toReturn.extend(getFiles(entry+"/"+file))
+  elif "ehthumbs.db" not in entry and "Thumbs.db" not in entry:
+    toReturn.append(entry)
+  return toReturn
+
+def playFiles(potentialFiles, volume):
+  subprocess.Popen('osascript -e "set Volume ' + str(volume) + '"', shell=True)
+  pos1 = random.randint(0,len(potentialFiles)-1)
+  pos2 = random.randint(0,len(potentialFiles)-1)
+  pos3 = random.randint(0,len(potentialFiles)-1)
+  print potentialFiles[pos1]
+  print potentialFiles[pos2]
+  print potentialFiles[pos3]
+  subprocess.Popen([
+    "/Applications/VLC.app/Contents/MacOS/VLC",
+    "--play-and-exit",
+    "--fullscreen",
+    potentialFiles[pos1],
+    potentialFiles[pos2],
+    potentialFiles[pos3]])
+  subprocess.Popen("osascript -e 'tell application \"VLC\"' -e 'activate' -e 'end tell'", shell=True)
+
+
+
+def startNightNight(directory, volume):
+  potentialFiles = getFiles(directory)
+  playFiles(potentialFiles, volume)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-w', '--towatch', default="default")
+args = parser.parse_args()
+
+
+
+try:
+  settingsFile = open(getSettingsFileName(), 'r')
+except IOError as e:
+  print "Couldn't open settings file"
+  exit(1)
+
+settings = json.loads(settingsFile.read())
+watch_options = settings['watch_options']
+towatch = None
+for watch_option in watch_options:
+  if watch_option['name'] == args.towatch:
+    towatch = watch_option['directory']
+    break
+
+if towatch != None:
+  startNightNight(towatch, settings["volume"])
+else:
+  print "No watch option with the name %s." % args.towatch
+  exit(1)
